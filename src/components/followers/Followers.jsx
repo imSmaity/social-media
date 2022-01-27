345ddf6662672c57c5142d223bec51a4294a289d
Suman <60940736+imSmaity@users.noreply.github.com>
2022-01-27 21:47:19 +0530

    design

--- src/components/followers/Followers.jsx
+++ src/components/followers/Followers.jsx
@@ -17,13 +17,14 @@ function Followers({loading,users,followers}) {
                         if(followers[i]===user._id){
                             i=followers.length
                             return(
-                                <div className='row mt-3' key={index}>
+                                <div className='row mt-4' key={index}>
                                     <div className='col-2'>
                                         <Link to={`/${user._id}`}> <img src={user.avatar} alt='pp' id='havatar'/></Link>
                                     </div>
                                     <div className="col-6">
-                                        <Link to={`/${user._id}`}>{`${user.fname} ${user.lname}`}</Link>
-                                        <div>{user._id}</div>
+                                        <Link to={`/${user._id}`} className='sp'>{`${user.fname} ${user.lname}`}
+                                            <div style={{marginTop:'-1vh',color:'gray',fontSize:'2.5vh'}}>@{user._id}</div>
+                                        </Link>
                                         <div>{user.bio}</div>
                                     </div>
                                     <div className="col-4">
